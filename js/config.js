8362d314ac450119e49f3104f26ada17d623e9e0
John Arackaparambil <jarackaparam@vmware.com>
2021-08-26 15:07:22 -0700

    Updated config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_Os1v8cz1t', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6mfe04rceeeqf2i5l9j6pg9dr4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
